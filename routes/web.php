dc6cf3c8b666a813254dc0631a79441ad695f0f0
Olle Källén <olle.kallen@hotmail.com>
2020-09-10 08:45:13 +0200

    catch all route for frontend

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::any('/{all}', function () {
+    return view('index');
+})->where('all', '^(?!api).*$');
