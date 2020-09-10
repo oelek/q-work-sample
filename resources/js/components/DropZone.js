b5ccc38df70ee38baff2f84b16b157e3401327be
Olle Källén <olle.kallen@hotmail.com>
2020-09-10 10:50:41 +0200

    configure dropzone

--- resources/js/components/DropZone.js
+++ resources/js/components/DropZone.js
@@ -4,13 +4,17 @@ import {useDropzone} from "react-dropzone";
 const DropZone = ({onSubmit, disabled}) => {
 
     const onDrop = useCallback(acceptedFiles => onSubmit(acceptedFiles), [])
-    const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+    const {getRootProps, getInputProps, isDragActive} = useDropzone({
+        onDropAccepted: onDrop,
+        disabled: disabled,
+        multiple: false
+    })
 
     return (
         <div
             className={'dropzone-container ' + (disabled ? ' --is-loading' : '') + (isDragActive ? ' --is-active' : '')} {...getRootProps()}>
             <input {...getInputProps()} />
-            {isDragActive ? <p>Drop the files here</p> :
+            {isDragActive ? <p>Drop the file here</p> :
                 <p>Drop files here</p>}
         </div>
     )
